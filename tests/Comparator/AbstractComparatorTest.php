4892fdfe981302fc0ab14307fd71aec22fa1b4d1
Fabian Schmengler <fschmengler@sgh-it.eu>
2015-04-06 17:40:06 +0200

    Add comment

--- tests/Comparator/AbstractComparatorTest.php
+++ tests/Comparator/AbstractComparatorTest.php
@@ -1,6 +1,16 @@
 <?php
 namespace SGH\Comparable\Test\Comparator;
 
+/**
+ * Base test case for comparators
+ * 
+ * @author Fabian Schmengler <fschmengler@sgh-it.eu>
+ * @copyright &copy; 2015 SGH informationstechnologie UG
+ * @license MIT
+ * @link http://opensource.org/licenses/mit-license.php
+ * @package Comparable
+ * @since 1.1.0
+ */
 abstract class AbstractComparatorTest extends \PHPUnit_Framework_TestCase
 {
     protected function assertCompareResult($expectedOrder, $actualOrder)
